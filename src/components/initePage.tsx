7947918a737144912033d81f0e30efe069ff89cd
sidgabrielmoura <sidgabrielmoura40@gmail.com>
2024-10-22 07:30:28 -0300

    navbar inited

--- src/components/initePage.tsx
+++ src/components/initePage.tsx
@@ -1,6 +1,9 @@
+import Navbar from "./navbar";
+
 export default function InitePage() {
     return (
         <>
+            <Navbar/>
             <main className="bg-[url(../assets/fundoInicial.svg)] w-screen h-screen bg-center bg-cover">
                 <div className="bg-[url(../assets/verdadeiraLogo.svg)] w-[500px] h-[500px] bg-center bg-cover bg-no-repet"></div>
             </main>
--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,9 +1,15 @@
 export default function Navbar(){
     return(
         <>
-            <main className="w-screen h-screen bg-zinc-900">
-                <div className="">
-
+            <main className="absolute w-full top-6 flex items-center justify-center">
+                <div className="h-9 w-[40%] bg-zinc-950/80 rounded-3xl flex items-center overflow-hidden">
+                    <div className="flex items-center justify-between w-full h-full">
+                        <button className="text-[14px] cursor-pointer hover:bg-zinc-800/90 transition-colors duration-200 text-zinc-50 font-medium tracking-[0.01rem] h-full w-full">Inicio</button>
+                        <button className="text-[14px] cursor-pointer hover:bg-zinc-800/90 transition-colors duration-200 text-zinc-50 font-medium tracking-[0.01rem] h-full w-full">Serviços</button>
+                        <button className="text-[14px] cursor-pointer hover:bg-zinc-800/90 transition-colors duration-200 text-zinc-50 font-medium tracking-[0.01rem] h-full w-full">Avaliações</button>
+                        <button className="text-[14px] cursor-pointer hover:bg-zinc-800/90 transition-colors duration-200 text-zinc-50 font-medium tracking-[0.01rem] h-full w-full">Sobre nós</button>
+                        <button className="text-[14px] cursor-pointer hover:bg-zinc-800/90 transition-colors duration-200 text-zinc-50 font-medium tracking-[0.01rem] h-full w-full">Contato</button>
+                    </div>
                 </div>
             </main>
         </>
